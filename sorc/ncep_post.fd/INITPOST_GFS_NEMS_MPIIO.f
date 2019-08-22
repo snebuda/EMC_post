c69b11166820d770477983fcc740da5c2bcb3644
Wen Meng <wen.meng@noaa.gov>
2019-08-22 18:16:07 +0000

    VLAB Issue #57000 -- Set sst as 271.5K when sea ice fraction is larger then 0.15.

--- sorc/ncep_post.fd/INITPOST_GFS_NEMS_MPIIO.f
+++ sorc/ncep_post.fd/INITPOST_GFS_NEMS_MPIIO.f
@@ -1981,7 +1981,11 @@
           qwbs(i,j)  = SPVAL ! GFS does not have inst latent heat flux
 !assign sst
           if (sm(i,j) /= 0.0) then
-             sst(i,j) = ths(i,j) * (pint(i,j,lp1)/p1000)**capa
+             if (sice(i,j,) >= 0.15)then
+               sst(i,j) = 271.5 * (pint(i,j,lp1)/p1000)**capa
+             else
+               sst(i,j) = ths(i,j) * (pint(i,j,lp1)/p1000)**capa
+             endif
           else
               sst(i,j) = spval
           endif
